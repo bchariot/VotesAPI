3af252a7ef2d906d51ed4c9279cb2780152073e2
bchariot <brucechariot@hotmail.com>
2020-10-08 10:12:30 -0400

    Add project files.

---
+++ VotesAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VotesAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
